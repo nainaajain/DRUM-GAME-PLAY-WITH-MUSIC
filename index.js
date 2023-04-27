cdc8e2148635acd4ab7e01245e7858602e9f852f
nainaajain <jainnaina202717@gmail.com>
2023-04-27 22:30:35 +0530

    version1

---
+++ index.js
@@ -0,0 +1,76 @@
+var numfordrums= document.querySelectorAll(".drum").length;
+// this is button detection 
+for (var i=0; i<numfordrums ;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var keyinnerhtml=this.innerHTML;
+         makesound(keyinnerhtml);
+         buttonAnimation(keyinnerhtml);
+        
+    });
+    
+
+}
+// this is for keypresses detection 
+document.addEventListener("keypress",function(event){
+
+    makesound(event.key);
+    buttonAnimation(event.key);
+    
+    })
+    // here key argument is for both button and keyboard presses
+   function makesound(key){ switch (key) {                   
+    case "w":
+        var audio= new Audio("sounds/tom-1.mp3");
+        audio.play();
+        
+        break;
+        case "a":
+        var audio= new Audio("sounds/tom-2.mp3");
+        audio.play();
+        
+        break;
+        case "s":
+        var audio= new Audio("sounds/tom-3.mp3");
+        audio.play();
+        
+        break;
+        case "d":
+        var audio= new Audio("sounds/tom-4.mp3");
+        audio.play();
+        
+        break;
+        case "j":
+        var audio= new Audio("sounds/snare.mp3");
+        audio.play();
+        
+        break;
+        case "k":
+        var audio= new Audio("sounds/crash.mp3");
+        audio.play();
+        
+        break;
+        case "l":
+        var audio= new Audio("sounds/kick-bass.mp3");
+        audio.play();
+        
+        break;
+        
+
+    default:
+        console.log(keyinnerhtml);
+        break;
+        
+}
+
+}
+//  now to give delay and to create animation for few seconds and it returns to its orginal 
+// state we use settimeout 
+    function buttonAnimation(currentkey){
+        var activebutton= document.querySelector("."+currentkey);
+        activebutton.classList.add("pressed");
+        setTimeout(function(){
+            activebutton.classList.remove("pressed");
+        },100);
+        
+    };
+
